a06577b2b3b61f3b8d1583b7b3cccdc72fc8b6e0
danaellecottier <danaelle@cottier.ca>
2022-01-27 22:39:02 -0500

    Initial Commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const hamburger = document.querySelector('.menu-hamburger');
+const pastille = document.querySelector('.pastille');
+
+const menu = document.querySelector('#navbar');
+
+hamburger.addEventListener('click', ()=> {
+    menu.style.display = "block";
+})
+
+pastille.addEventListener('click', ()=> {
+    menu.style.display = "block";
+})
